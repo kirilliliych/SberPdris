b4b915212ccc3b110b6c8326fba31bf4986f5a97
Ilichev Kirill <ilichev.kv@phystech.edu>
2024-12-11 02:47:32 +0300

    app commit

---
+++ task4/app/src/main/java/org/example/BruhClass.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class BruhClass {
+    public static String authentication(int value) {
+        return value == 52 ? "Glory to Saint-Petersburg and this is our city" :
+                "...? Anything here?...";
+    }
+}
